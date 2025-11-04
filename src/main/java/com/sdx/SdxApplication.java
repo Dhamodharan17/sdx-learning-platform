7f9d0900b0a4fea11dc0c3ea3aa1e98938215904
DSUNDAR <dhamodharan.sundararjan@mercedes-benz.com>
2025-11-04 15:03:16 +0530

    first commit

---
+++ src/main/java/com/sdx/SdxApplication.java
@@ -0,0 +1,13 @@
+package com.sdx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdx/SdxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
